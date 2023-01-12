a034769a6d58f65a5252f5a11d112ae3880da55f
Genisc13 <genis.castillo@estudiantat.upc.edu>
2023-01-12 23:42:07 +0100

    Añadido el codigo para hacer que cambie el idioma al darle al botón de cambio de idioma correctamente

--- app/src/main/java/edu/upc/dsa/andoroid_dsa/activities/User_conf_activity.java
+++ app/src/main/java/edu/upc/dsa/andoroid_dsa/activities/User_conf_activity.java
@@ -1,10 +1,13 @@
 package edu.upc.dsa.andoroid_dsa.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import java.util.List;
@@ -14,13 +17,14 @@ import edu.upc.dsa.andoroid_dsa.Api;
 import edu.upc.dsa.andoroid_dsa.R;
 import edu.upc.dsa.andoroid_dsa.RetrofitClient;
 import edu.upc.dsa.andoroid_dsa.models.Gadget;
+import edu.upc.dsa.andoroid_dsa.models.User;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class User_conf_activity extends AppCompatActivity {
 
-    String language="en";
+    String language="en-us";
     Api APIservice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,7 @@ public class User_conf_activity extends AppCompatActivity {
     }
     public void change_language(View view){
         APIservice = RetrofitClient.getInstance().getMyApi();
-        if(language.equals("en")){
+        if(language.equals("en-us")){
             Call<Void> call = APIservice.changelng("es");
             call.enqueue(new Callback<Void>() {
                 @Override
@@ -42,16 +46,11 @@ public class User_conf_activity extends AppCompatActivity {
 
                 }
             });
-            Locale localizacion = new Locale("es", "ES");
-
-            Locale.setDefault(localizacion);
-            Configuration config = new Configuration();
-            config.setLocale(localizacion);
-            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+            setLocale("es");
             this.setLanguage("es");
         }
         else{
-            Call<Void> call = APIservice.changelng("en");
+            Call<Void> call = APIservice.changelng("en-us");
             call.enqueue(new Callback<Void>() {
                 @Override
                 public void onResponse(Call<Void> call, Response<Void> response) {
@@ -63,13 +62,8 @@ public class User_conf_activity extends AppCompatActivity {
 
                 }
             });
-            Locale localizacion = new Locale("en", "US");
-
-            Locale.setDefault(localizacion);
-            Configuration config = new Configuration();
-            config.setLocale(localizacion);
-            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-            this.setLanguage("en");
+            setLocale("default");
+            this.setLanguage("en-us");
         }
 
     }
@@ -77,5 +71,23 @@ public class User_conf_activity extends AppCompatActivity {
 
     public void setLanguage(String language) {
         this.language = language;
+    }
+
+    private void setLocale(String language) {
+        Resources resources = getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = new Locale(language);
+        resources.updateConfiguration(configuration, metrics);
+        onConfigurationChanged(configuration);
+        Intent intentRegister = new Intent(User_conf_activity.this, User_conf_activity.class);
+        User_conf_activity.this.startActivity(intentRegister);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfiguration) {
+        super.onConfigurationChanged(newConfiguration);
+        //set strings from resources
+
     }
 }
